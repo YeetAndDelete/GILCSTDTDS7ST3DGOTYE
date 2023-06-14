d32071e09ea51208410fad61c3521913306078cd
Calum223 <allendalyc@limestone.on.ca>
2023-06-14 14:34:53 -0400

    HealthBarSemi
    Almost functioning, needs to be synced w/ the balloon disappearin

---
+++ FinalProject/HealthBar.java
@@ -0,0 +1,41 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class HealthBar here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HealthBar extends Actor
+{
+    int health = 4;
+    int healthBarWidth = 80;
+    int healthBarHeight = 15;
+    int pixelsPerHealthPoint = (int)healthBarWidth / health;
+    /**
+     * Act - do whatever the HealthBar wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void LoseHealth(){
+    health--;
+    }
+    public void HealthBar(){
+    update();
+    }
+    public void act()
+    {
+        update();
+    }
+    public void update(){
+    
+    setImage(new GreenfootImage(healthBarWidth + 2, healthBarHeight +2));
+        GreenfootImage myImage = getImage();
+        myImage.setColor(Color.WHITE);
+        myImage.drawRect(0,0, healthBarWidth + 1,healthBarHeight + 1);
+        myImage.setColor(Color.RED);
+        myImage.fillRect(1,1, health*pixelsPerHealthPoint, healthBarHeight);
+        
+    }
+    
+    
+}
