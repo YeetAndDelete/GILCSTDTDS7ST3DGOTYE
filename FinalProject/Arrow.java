2976858da46c991e87319f7812a28f97fb464a59
YeetAndDelete <zack.brown234@gmail.com>
2023-06-15 14:20:50 -0400

    Button spawner
    the wave system is running out of time and we need more features

--- FinalProject/Arrow.java
+++ FinalProject/Arrow.java
@@ -7,37 +7,15 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @version (a version number or a date)
  */
 public class Arrow extends Actor
-{   int waveCount=0;
+{   
     int balloonCount=0;
-    int timer = 0;
     boolean waveProgress=false;
-    boolean allowSpawn=true;
     public void act()
     {
         if (Greenfoot.mouseClicked(this)){
-            waveCount++;
+            balloonCount++;
+            ((GameScreen)getWorld()).addObject (new Latex(),400,800);
         }
-        if(waveCount==1 && waveProgress==false){
-            for (int x=0; x<5; x++ ){
-                waveCount=2;
-                ((GameScreen)getWorld()).addObject (new Latex(),400,800);
-                if (x==5){
-                    waveProgress=false;
 
-                }
-            }
-        }
-    }
-
-    public void antiSpam(){
-        //makes a timer for time between firing so enemy wont spam 60 bullets a second 
-        timer++;
-        if (timer>30){
-            allowSpawn =false;
-            if(timer>60){
-                allowSpawn=true;
-                timer=0;
-            }
-        }
     }
 }
